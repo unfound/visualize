9578c1ac37172ce4447f7be7aec2f89f04096fef
unfound <448217185@qq.com>
2021-09-15 18:05:28 +0800

    添加stats显示帧率

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,23 @@
   <router-view></router-view>
 </template>
 
+<script setup lang="ts">
+import Stats from 'stats.js'
+
+const stats = new Stats()
+stats.showPanel(0)
+document.body.appendChild(stats.dom)
+
+function animate () {
+  stats.begin()
+  stats.end()
+
+  requestAnimationFrame(animate)
+}
+
+requestAnimationFrame(animate)
+</script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
